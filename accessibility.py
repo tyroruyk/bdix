8192bdadcf4a5bfdd3d5533cb3f49584cf0d6df5
Avishek Dutta <avishekdutta531@gmail.com>
2024-07-27 20:07:38 +0600

    first commit

---
+++ accessibility.py
@@ -0,0 +1,12 @@
+import requests
+from tqdm import tqdm
+
+def accessibility(url):
+    try:
+        # simulate a long-running operation with tqdm
+        for _ in tqdm(range(1), desc="Pinging {0}".format(url)):
+            response = requests.get(url, timeout=5)
+        response.raise_for_status()
+        return True
+    except:
+        return False
---
+++ app.py
@@ -0,0 +1,23 @@
+from accessibility import accessibility
+from readFile import readFile
+
+urls = readFile("bdixServers.txt") # credit goes to PC Builder Bangladesh for the server list
+workingUrls = []
+
+for url in urls:
+    if accessibility(url):
+        workingUrls.append(url)
+
+if len(workingUrls) != 0:
+    
+    print("List of working servers: ", end=" ")
+    for url in workingUrls:
+        print(url, end=' ')
+
+    print("\n{0} servers are working out of {1} servers".format(len(workingUrls), len(urls)))
+
+else:
+    print("No server is working :(")
+
+print("Press any key to quit...", end='')
+input()
