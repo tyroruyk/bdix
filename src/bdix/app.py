fc790ee1757c6759e2380d66434a7f13abc5275a
Avishek Dutta <avishekdutta531@gmail.com>
2024-07-28 11:05:32 +0600

    fixed

--- src/bdix/app.py
+++ src/bdix/app.py
@@ -1,5 +1,5 @@
-from accessibility import accessibility
-from readFile import readFile
+from bdix.accessibility import accessibility
+from bdix.readFile import readFile
 from os import path
 
 base_path = path.dirname(path.abspath(__file__))
